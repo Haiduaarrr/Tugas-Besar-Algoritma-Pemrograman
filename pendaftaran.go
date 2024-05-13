3d94304199a42f371f2694bc2b91cbaacf1fc41a
Haidar Sayyid Ramadhan <142973591+Haiduaarrr@users.noreply.github.com>
2024-05-13 10:57:53 +0700

    Update pendaftaran.go

--- pendaftaran.go
+++ pendaftaran.go
@@ -5,12 +5,12 @@ import "fmt"
 const nmax int = 100
 
 type calonMHS struct {
-	nama, domisili, jurusan, asalSekola string
+	nama, domisili, jurusan, asalSekolah string
 	nilaiTes                            int
 }
 
 type tabCalon [nmax]calonMHS
-
+ 
 func menu() {
 	// Menampilkan pilihan menu
 	fmt.Println()
@@ -24,6 +24,13 @@ func menu() {
 	fmt.Println()
 }
 
-func isicCalon() {
+func isicCalon(mhs *tabCalon, *n int) {
+	/*
+		IS: Array mhs terdefinisi sembarang
+  		FS: Array mhs terisi data nama, jurusan, domisili, asal sekolah, dan nilai tes
+ 	*/
+	var M calonMHS
 
+	for M.nama != "selesai" {
+		
 }
