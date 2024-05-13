1eaa07c8ae3d081aad8aaf693512559ba6dc3f7f
Haidar Sayyid Ramadhan <142973591+Haiduaarrr@users.noreply.github.com>
2024-05-13 10:40:20 +0700

    Add files via upload

---
+++ pendaftaran.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+const nmax int = 100
+
+type calonMHS struct {
+	nama, domisili, jurusan, asalSekola string
+	nilaiTes                            int
+}
+
+type tabCalon [nmax]calonMHS
+
+func menu() {
+	// Menampilkan pilihan menu
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+}
+
+func isicCalon() {
+
+}
